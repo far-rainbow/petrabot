51afd9d7aa58e4d1f91dd044ab3845f6ac46b7aa
Pavel E. Petrov <p_13@mail.ru>
2020-10-10 03:20:05 +0300

    soft refactor + proxy added (for test in nets with blocked TG)
    Signed-off-by: Pavel E. Petrov <p_13@mail.ru>

--- petrabot.py
+++ petrabot.py
@@ -1,8 +1,7 @@
+''' PetraBot Telegram bot logic '''
 import os
 import subprocess
 from datetime import datetime, timezone
-from hashlib import sha3_256 as hash
-import requests
 import telebot
 import httpx
 
@@ -13,9 +12,25 @@ from sqlalchemy import create_engine
 
 ROOTDIR = os.path.dirname(os.path.abspath(__file__))
 API_TOKEN = '1067546684:AAEyYPuY1m9cIQ7OMVux71rBy6mS6pC9EAg'
-bot = telebot.TeleBot(API_TOKEN)
+BOT = telebot.TeleBot(API_TOKEN)
+
+STATIC_PROXY_LIST = ['http://bot:qwepasdlzxcm@3.127.50.231:9000']
+
+START_MESSAGE = '''
+Hi there, I am Kamenka SU Bot.
+I am here to enjoy you with some stuff.
+Tap /help to get more info.'''
+
+HELP_MESSAGE = '''
+Commands:
+/face -- get fake face
+/fortune -- get random cookie text
+Chat with BOT to get some contex fortunes...
+'''
 
 BASE = declarative_base()
+
+
 class MessageRecord(BASE):
     ''' ORM '''
     __tablename__ = 'messages'
@@ -25,56 +40,68 @@ class MessageRecord(BASE):
     answer = Column(String)
     time = Column(DateTime)
 
-engine = create_engine(f'sqlite:///petrabot.db', echo=False)
-BASE.metadata.create_all(engine)
-session = sessionmaker(bind=engine)()
 
-def push_to_db(message,answer=None):
-    print(message.from_user.username,' : ',message.text)
+ENGINE = create_engine(f'sqlite:///petrabot.db', echo=False)
+BASE.metadata.create_all(ENGINE)
+SESSION = sessionmaker(bind=ENGINE)()
+
+
+def push_to_db(message, answer=None):
+    ''' save username,query and BOT answer (optionaly) into db '''
+    print(message.from_user.username, ' : ', message.text)
     if answer is not None:
-        print('Answer: ',answer.decode('utf-8'))
-    mrec = MessageRecord(name=message.from_user.username,query=message.text,answer=answer,time=datetime.now(timezone.utc))
-    session.add(mrec)
-    session.commit()
+        print('Answer: ', answer.decode('utf-8'))
+    mrec = MessageRecord(name=message.from_user.username, query=message.text,
+                         answer=answer, time=datetime.now(timezone.utc))
+    SESSION.add(mrec)
+    SESSION.commit()
 
-# find citates with user words
+
 def finder(uid, cmd=''):
+    ''' find fortune via shell subprocess script '''
     try:
-        fortune = subprocess.check_output(['%s/finder.sh' % ROOTDIR, '%s' % uid, '%s' % cmd.lower()])
-    except:
-        fortune = subprocess.check_output(['/usr/games/fortune', '-a', '/usr/share/games/fortunes/'])
+        fortune = subprocess.check_output(
+            ['%s/finder.sh' % ROOTDIR, '%s' % uid, '%s' % cmd.lower()])
+    except subprocess.SubprocessError:
+        fortune = subprocess.check_output(
+            ['/usr/games/fortune', '-a', '/usr/share/games/fortunes/'])
     return fortune
+
 
 def get_face():
     ''' html response getter '''
     headers = {'User-Agent': 'Mozilla/5.0 (compatible; YandexMetrika/2.0; \
                 +http://yandex.com/bots yabs01)'}
-    with httpx.Client(headers=headers, timeout=10.0) as sess:
+    with httpx.Client(proxies=STATIC_PROXY_LIST[0], headers=headers, timeout=10.0) as sess:
         return sess.get(url='https://thispersondoesnotexist.com/image').content
 
-@bot.message_handler(commands=['help', 'start','stop','face','fortune'])
-def send_welcome(message):
 
+@BOT.message_handler(commands=['help', 'start', 'stop', 'face', 'fortune'])
+def send_welcome(message):
+    ''' BOT commands logic '''
     answer = None
     if message.text == '/start':
-        bot.reply_to(message, f"Hi there, I am Kamenka SU Bot. I am here to enjoy you with some stuff. Tap /help to get more info.")
+        BOT.reply_to(
+            message, START_MESSAGE)
     elif message.text == '/help':
-        bot.reply_to(message,f'Commands:\n/face -- get fake face\n/fortune -- get random cookie text\nChat with bot to get some contex fortunes...')
+        BOT.reply_to(
+            message, HELP_MESSAGE)
     elif message.text == '/stop':
-        bot.reply_to(message, 'Z-z-z-z...')
+        BOT.reply_to(message, 'Z-z-z-z...')
     elif message.text == '/face':
-        bot.send_photo(message.chat.id, get_face())
+        BOT.send_photo(message.chat.id, get_face())
     elif message.text == '/fortune':
         answer = finder(message.from_user.username)
-        bot.reply_to(message, answer)
-    push_to_db(message,answer)
+        BOT.reply_to(message, answer)
+    push_to_db(message, answer)
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
+
+@BOT.message_handler(func=lambda message: True)
 def echo_message(message):
+    ''' user text logic '''
+    answer = finder(message.from_user.username, message.text)
+    BOT.reply_to(message, answer)
+    push_to_db(message, answer)
 
-    answer = finder(message.from_user.username,message.text)
-    bot.reply_to(message,answer)
-    push_to_db(message,answer)
 
-bot.polling()
+BOT.polling()
