771ecbeddf14b0a02cbf5c55b2c057c75f4af973
Pavel E. Petrov <p_13@mail.ru>
2020-10-10 20:20:06 +0300

    httpx to async, photos fixed
    Signed-off-by: Pavel E. Petrov <p_13@mail.ru>

--- petrabot.py
+++ petrabot.py
@@ -2,10 +2,11 @@
 import os
 import sys
 import subprocess
+import asyncio
 from datetime import datetime, timezone
+import itertools
 import telebot
 import httpx
-import itertools
 import proxyscrape
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -15,13 +16,9 @@ from sqlalchemy import create_engine
 
 ROOTDIR = os.path.dirname(os.path.abspath(__file__))
 API_TOKEN = '1067546684:AAEyYPuY1m9cIQ7OMVux71rBy6mS6pC9EAg'
-BOT = telebot.TeleBot(API_TOKEN)
+BOT = telebot.AsyncTeleBot(API_TOKEN)
 PROXYNUM = 32
-
-if sys.platform == 'win32':
-    WIN32 = True
-else:
-    WIN32 = False
+WIN32 = bool(sys.platform == 'win32')
 
 START_MESSAGE = '''
 Hi there, I am Kamenka SU Bot.
@@ -52,13 +49,13 @@ ENGINE = create_engine(f'sqlite:///petrabot.db', echo=False)
 BASE.metadata.create_all(ENGINE)
 SESSION = sessionmaker(bind=ENGINE)()
 
-collector = proxyscrape.create_collector(
+COLLECTOR = proxyscrape.create_collector(
     'default', 'https', refresh_interval=10)
 
 
 def get_proxy_scrape(num):
     ''' proxy scraper '''
-    proxy = collector.get_proxies({'anonymous': True})
+    proxy = COLLECTOR.get_proxies({'anonymous': True})
     proxy_dict = []
     for _ in proxy[:num]:
         proxy_item = {'http': 'http://' + _[0] + ':' + _[1]}
@@ -66,7 +63,7 @@ def get_proxy_scrape(num):
     return proxy_dict
 
 
-proxy_loop_iterator = itertools.cycle(get_proxy_scrape(PROXYNUM))
+PROXYLOOPITERATOR = itertools.cycle(get_proxy_scrape(PROXYNUM))
 
 
 def push_to_db(message, answer=None):
@@ -94,14 +91,14 @@ def finder(uid, cmd=''):
     return fortune
 
 
-def get_face():
+async def get_face():
     ''' html response getter '''
     headers = {'User-Agent': 'Mozilla/5.0 (compatible; YandexMetrika/2.0; \
                 +http://yandex.com/bots yabs01)'}
-    proxy = next(proxy_loop_iterator, None)
-    print(f'{proxy=}')
-    with httpx.Client(proxies=proxy, headers=headers, timeout=10.0) as sess:
-        return sess.get(url='https://thispersondoesnotexist.com/image').content
+    proxy = next(PROXYLOOPITERATOR, None)
+    async with httpx.AsyncClient(proxies=proxy, http2=True, headers=headers, timeout=10.0) as sess:
+        result = await sess.get(url='https://thispersondoesnotexist.com/image')
+        return {'proxy': proxy, 'result': result.content}
 
 
 @BOT.message_handler(commands=['help', 'start', 'stop', 'face', 'talk'])
@@ -115,7 +112,7 @@ def send_welcome(message):
     elif message.text == '/stop':
         BOT.reply_to(message, 'Z-z-z-z...')
     elif message.text == '/face':
-        BOT.send_photo(message.chat.id, get_face())
+        BOT.send_photo(message.chat.id, asyncio.run(get_face()))
     elif message.text == '/talk':
         answer = finder(message.from_user.username)
         BOT.reply_to(message, answer)
