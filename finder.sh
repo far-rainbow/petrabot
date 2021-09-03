44fd1d475d4ea03930340fbde18258f02257c1c9
Pavel E. Petrov <9624391+far-rainbow@users.noreply.github.com>
2021-09-04 00:10:47 +0300

    Update finder.sh

--- finder.sh
+++ finder.sh
@@ -22,8 +22,8 @@ fi
 cd $dirid
 rm xx* > /dev/null 2>&1
 
-/usr/games/fortune -i -m "\(^\| \)\($2\)" && /usr/games/fortune -i -m "\(^\| \)\($2\)" ru > /dev/stdout 2>/dev/null | csplit - -sz --suppress-matched /%/ {*}
-#/usr/games/fortune -m $2 > /dev/stdout 2>/dev/null | csplit - -sz --suppress-matched /%/ {*}
+/usr/games/fortune -i -m "\(^\| \)\($2\)" > /dev/stdout 2>/dev/null | csplit - -sz --suppress-matched /%/ {*}
+/usr/games/fortune -i -m "\(^\| \)\($2\)" ru > /dev/stdout 2>/dev/null | csplit - -sz --suppress-matched /%/ {*}
 
 ### OK, now tmp/uid is full (or empty if none is found) of files
 ### next let  the python script done the things over with only one choosen from all the bunch
