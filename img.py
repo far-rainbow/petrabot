18bc62addd2614cf6222d007d6ce9d214add0260
Pavel E. Petrov <admin@kamenka.su>
2021-09-10 06:04:32 +0300

    refact
    Signed-off-by: Pavel E. Petrov <admin@kamenka.su>

--- img.py
+++ img.py
@@ -2,8 +2,9 @@ import io
 import glob
 import random
 import textwrap
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
 from copy import deepcopy
+
 
 class Img():
     '''
@@ -18,18 +19,18 @@ class Img():
     TEXT_STROKE_COLOR = 'black'
     TEXT_STROKE_WIDTH = 2
 
-    def __init__(self,path):
+    def __init__(self, path):
         self.pics = self.loadAllPics(path)
         self.font = ImageFont.truetype("Lobster-Regular.ttf", self.TEXT_FONT_SIZE)
     
-    def loadAllPics(self,path):
+    def loadAllPics(self, path):
         '''
         private
         preload all images with onfly resizing to global width/height attrs
         :param dir path to fetch pics from
         :returns: a list with Image object loaded from path dir
         '''
-        pic_pathes = glob.glob(path+'*.jpg') + glob.glob(path+'*.png')
+        pic_pathes = glob.glob(path + '*.jpg') + glob.glob(path + '*.png')
         pics = list()
         for _ in pic_pathes:
             ratio = None
@@ -39,19 +40,19 @@ class Img():
             if img.height > self.MAX_HEIGHT or img.width > self.MAX_WIDTH:
                 height_ratio = 1 / (img.height / self.MAX_HEIGHT)
                 width_ratio = 1 / (img.width / self.MAX_WIDTH)
-                ratio = min(height_ratio,width_ratio)
+                ratio = min(height_ratio, width_ratio)
             elif img.height < self.MAX_HEIGHT or img.width < self.MAX_WIDTH:
                 height_ratio = self.MAX_HEIGHT / img.height
                 width_ratio = self.MAX_WIDTH / img.width
-                ratio = min(height_ratio,width_ratio)
+                ratio = min(height_ratio, width_ratio)
             if ratio:
-                img = img.resize(size=(int(img.width*ratio),int(img.height*ratio)))
+                img = img.resize(size=(int(img.width * ratio), int(img.height * ratio)))
                 print(f'Resized with {ratio} ratio. New size is {img.size}')
             pics.append(img)
         print(f'{len(pics)} pics loaded...')
         return pics
 
-    def getBytes(self,img,qlty=75):
+    def getBytes(self, img, qlty=75):
         '''
         convert Image into JPEG byte array
         :param img: Image obj to convert
@@ -60,7 +61,7 @@ class Img():
         '''
         imgByteArr = io.BytesIO()
         imgRGB = img.convert(mode='RGB')
-        imgRGB.save(imgByteArr, format='JPEG',quality=qlty)
+        imgRGB.save(imgByteArr, format='JPEG', quality=qlty)
         imgRGB = imgByteArr.getvalue()
         return imgRGB
 
@@ -71,7 +72,7 @@ class Img():
         '''
         return deepcopy(random.choice(self.pics))
 
-    async def getRandomImageWithText(self,text):
+    async def getRandomImageWithText(self, text):
         '''
         public
         Get random image from pics list, draw a text on it, convert it to JPEG
@@ -81,10 +82,10 @@ class Img():
         '''
         imgRGB = await self.getRandomImage()
         draw = ImageDraw.Draw(imgRGB)
-        text_lines = textwrap.wrap(text.decode('utf-8'),self.TEXT_MAX_CHARS_PER_LINE)
+        text_lines = textwrap.wrap(text.decode('utf-8'), self.TEXT_MAX_CHARS_PER_LINE)
         v_pos = self.TEXT_START_V_POS
         for line in text_lines:
-            font_width,font_height = self.font.getsize(line)
-            draw.text((32,v_pos),line,(255,255,255),font=self.font,stroke_width=self.TEXT_STROKE_WIDTH, stroke_fill=self.TEXT_STROKE_COLOR)
+            font_width, font_height = self.font.getsize(line)
+            draw.text((32, v_pos), line, (255, 255, 255), font=self.font, stroke_width=self.TEXT_STROKE_WIDTH, stroke_fill=self.TEXT_STROKE_COLOR)
             v_pos += font_height
         return self.getBytes(imgRGB)
