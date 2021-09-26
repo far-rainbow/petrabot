3cb1e57f15581ca6c01ea3629283088de7d9055c
Pavel E. Petrov <9624391+far-rainbow@users.noreply.github.com>
2021-09-26 21:28:59 +0300

    Update pip_upgrade_all.py

--- pip_upgrade_all.py
+++ pip_upgrade_all.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import pkg_resources
-from subprocess import call
-
-packages = [dist.project_name for dist in pkg_resources.working_set]
-call("pip install --upgrade " + ' '.join(packages), shell=True)
-=======
 ''' mass modules updater '''
 from subprocess import call
 from pkg_resources import working_set
 
 packages = [dist.project_name for dist in working_set]
 call("pip install --upgrade " + ' '.join(packages), shell=True)
->>>>>>> refs/heads/master
